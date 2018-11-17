e64ce0fda8d87f9dfe0ecbb058d2a2052a3250c6
Mengjie <mengjie.zhao@outlook.com>
2018-11-18 00:35:40 +0100

    add setup

---
+++ practice/setup.sh
@@ -0,0 +1,8 @@
+# setting up environment
+pip install virtualenv
+virtualenv --no-site-packages ./sentiment-analaysis-vgc
+
+# install requirements
+source ./sentiment-analaysis-vgc/bin/activate
+pip install -r requirements.txt
+
